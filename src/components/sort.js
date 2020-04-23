1db424be75c9d333936781643e9196cc3169224c
Nastya <zelenenkaya@gmail.com>
2020-04-23 10:29:13 +0300

    Add Sort types

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,8 +1,18 @@
-import {createSortTemplate} from '../templates/sort.js';
+import {createSortTemplate, SortType} from '../templates/sort.js';
 import AbstructComponent from './abstract-component.js';
 
 export default class Sort extends AbstructComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
   getTemplate() {
     return createSortTemplate();
   }
+
+  getSortType() {
+    return this._currentSortType;
+  }
+
+
 }
