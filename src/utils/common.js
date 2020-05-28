a3c3b2fb5e91f5495c1e8d60ae6b10b6edf3ba7a
Nastya <zelenenkaya@gmail.com>
2020-05-28 11:10:58 +0300

    add humanize date in comments

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,35 +1,5 @@
 import moment from "moment";
 
-const getRandomArrayItem = (array) => {
-  const randomIndex = getRandomIntegerNumber(0, array.length);
-
-  return array[randomIndex];
-};
-
-const getRandomIntegerNumber = (min, max) => {
-  return min + Math.floor(Math.random() * (max - min));
-};
-
-const getRandomArray = (baseArray, count = baseArray.length) => {
-  const newArray = [];
-  for (let i = 0; i < baseArray.length && i < count; i++) {
-    if (Math.random() > 0.5) {
-      newArray.push(baseArray[i]);
-    }
-  }
-  return newArray;
-};
-
-const getRandomDate = () => {
-  const targetDate = new Date();
-  const sign = -1;
-  const diffValue = sign * getRandomIntegerNumber(0, 8);
-
-  targetDate.setDate(targetDate.getDate() + diffValue);
-
-  return targetDate;
-};
-
 const getFilmDuration = (minutes) => {
   const duration = moment.duration(minutes, `minutes`);
   const durationHours = duration.hours();
@@ -42,17 +12,18 @@ const getFilmDuration = (minutes) => {
   }
 };
 
-const formatReleaseDate = (date) => {
+const formatReleaseDateOnlyYear = (date) => {
   return moment(date).format(`YYYY`);
 };
 
-const formatDate = (date) => {
+const formatDateFullDate = (date) => {
   return moment(date).format(`DD MMMM YYYY`);
 };
 
 const formatCommentDate = (date) => {
-  return moment(date).format(`YYYY/MM/DD hh:mm`);
+  const diff = moment(date).diff(moment());
+  return moment.duration(diff).humanize(true);
 };
 
 
-export {getRandomIntegerNumber, getRandomArrayItem, getFilmDuration, getRandomArray, formatDate, getRandomDate, formatCommentDate, formatReleaseDate};
+export {getFilmDuration, formatDateFullDate, formatCommentDate, formatReleaseDateOnlyYear};
